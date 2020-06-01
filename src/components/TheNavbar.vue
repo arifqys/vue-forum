86f2210378c804b65bf3d69af13eb1ad645056ba
Ahmad Rifqy Syarwani <arifqys@gmail.com>
2020-06-02 06:31:11 +0700

    improve ux with custom directives #17

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,21 +1,21 @@
 <template>
-   <header class="header" id="header">
+   <header class="header" id="header" v-click-outside="closeMobileNavbar" v-handle-scroll="closeMobileNavbar">
     <router-link
       :to="{name: 'Home'}"
       class="logo"
     >
       <img src="../assets/img/vueschool-logo.svg">
     </router-link>
-    <div class="btn-hamburger">
+    <div class="btn-hamburger" @click="mobileNavOpen = !mobileNavOpen">
       <!-- use .btn-humburger-active to open the menu -->
       <div class="top bar"></div>
       <div class="middle bar"></div>
       <div class="bottom bar"></div>
     </div>
     <!-- use .navbar-open to open nav -->
-    <nav class="navbar">
+    <nav class="navbar" :class="{'navbar-open': mobileNavOpen}">
       <ul v-if="user">
-        <li class="navbar-user">
+        <li class="navbar-user" v-click-outside="closeUserDropdown">
           <a @click.prevent="userDropdownOpen = !userDropdownOpen">
             <img class="avatar-small" :src="user.avatar">
             <span>
@@ -35,12 +35,14 @@
             </ul>
           </div>
         </li>
+        <li @click.prevent="mobileNavOpen = !mobileNavOpen" class="navbar-mobile-item"><router-link :to="{name: 'Profile'}">View Profile</router-link></li>
+        <li @click.prevent="mobileNavOpen = !mobileNavOpen" class="navbar-mobile-item"><a @click.prevent="$store.dispatch('auth/signOut')">Sign Out</a></li>
       </ul>
       <ul v-else>
-        <li class="navbar-item">
+        <li @click.prevent="mobileNavOpen = !mobileNavOpen" class="navbar-item">
           <router-link :to="{name: 'SignIn'}">Sign In</router-link>
         </li>
-        <li class="navbar-item">
+        <li @click.prevent="mobileNavOpen = !mobileNavOpen" class="navbar-item">
           <router-link :to="{name: 'Register'}">Register</router-link>
         </li>
       </ul>
@@ -50,17 +52,32 @@
 
 <script>
 import {mapGetters} from 'vuex'
+import clickOutside from '@/directives/click-outside'
+import handleScroll from '@/directives/handle-scroll'
 
 export default {
+  directives: {
+    clickOutside,
+    handleScroll
+  },
   data () {
     return {
-      userDropdownOpen: false
+      userDropdownOpen: false,
+      mobileNavOpen: false
     }
   },
   computed: {
     ...mapGetters({
       'user': 'auth/authUser'
     })
+  },
+  methods: {
+    closeUserDropdown () {
+      this.userDropdownOpen = false
+    },
+    closeMobileNavbar () {
+      this.mobileNavOpen = false
+    }
   }
 }
 </script>
